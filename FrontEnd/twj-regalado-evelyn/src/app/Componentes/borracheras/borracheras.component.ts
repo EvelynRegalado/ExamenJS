7fd17332645c59e18ff45f1d97a89f1c8aba44b1
Evelyn Regalado <evelyn.regalado@epn.edu.ec>
2017-06-15 15:21:56 -0500

    Ejercicio 1- parte7

--- FrontEnd/twj-regalado-evelyn/src/app/Componentes/borracheras/borracheras.component.ts
+++ FrontEnd/twj-regalado-evelyn/src/app/Componentes/borracheras/borracheras.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {BorracheraClass} from "../../Clases/BorracheraClass";
+import {Http} from "@angular/http";
 
 @Component({
   selector: 'app-borracheras',
@@ -15,9 +16,39 @@ export class BorracherasComponent implements OnInit {
   borracheras: BorracheraClass[] = [];
   nuevoBorrachera: BorracheraClass = new BorracheraClass();
 
-  constructor() { }
+  constructor(public _http:Http) { }
 
   ngOnInit() {
+     this._http.get('http://localhost:1337/Borrachera')
+      .subscribe(
+        respuesta => {
+          let respJson:BorracheraClass[]= respuesta.json();
+          console.log("respuesta json:", respJson);
+          this.borracheras=respJson;
+          console.log("Borracheras: ", this.borracheras);
+        },
+        error => {
+          console.log("Error", error)
+        }
+      )
   }
+  crearBorrachera() {
+    console.log("Guardo los datos");
+    /*let usuario= {
+     nombre: this.nuevoUsuario.nombre
+     }*/
+    this._http.post('http://localhost:1337/Borrachera', this.nuevoBorrachera)
+      .subscribe(
+        respuesta => {
+          let respuestaJson = respuesta.json();
+          console.log('respuestaJson: ', respuestaJson);
+          this.borracheras.push(respuestaJson);
+        },
+        error => {
+          console.log("Error", error)
+        }
+      )
+  }
+
 
 }

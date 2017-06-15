274ff9feb023462d8fdabda06c840570427c5d43
Evelyn Regalado <evelyn.regalado@epn.edu.ec>
2017-06-15 16:09:45 -0500

    Interface de Harry Potter

--- FrontEndProblem2/twj-examen-regalado-evelyn/src/app/Componentes/Interfaces/HarryPotter.ts
+++ FrontEndProblem2/twj-examen-regalado-evelyn/src/app/Componentes/Interfaces/HarryPotter.ts
@@ -1,3 +1,21 @@
 /**
  * Created by Pc Xtreme on 15/06/2017.
  */
+export interface PeliculaHarryPotter{
+  name: string,
+  species: string,
+  gender: string,
+  house: string,
+  dateOfBirth: date,
+  yearOfBirth: number,
+  ancestry: string,
+  eyeColour: string,
+  hairColour: string,
+  wand: string[]
+  patronus: string,
+  hogwartsStudent: boolean,
+  hogwartsStaff: boolean,
+  actor: string,
+  alive: boolean,
+  image: string
+}
